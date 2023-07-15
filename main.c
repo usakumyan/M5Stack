536f702258264455bac2f3f8626eed2953f31f30
takahashi <takahashi@ise.osaka-sandai.ac.jp>
2023-07-15 14:47:51 +0900

    [Update] main.c

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef float DTYPE;
+typedef short IDX;
+#include "fft.h"
+
+#define FFTSIZE (256)
 int main(int argc,char **argv[])
 {
+  DTYPE pRe[FFTSIZE];
+  DTYPE pIm[FFTSIZE];
+
+  for (IDX i=0;i<256;i++) {
+    pRe[i]=pRe[i]*pWindow[i]; // pWindow[] is define in fft.h
+  }
+  RealFFTExec256(pRe,pIm);
+  
   return 0;
 }
